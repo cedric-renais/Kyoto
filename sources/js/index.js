5ea015b258b283ee5c6ab8138a49731f8f59b55a
Cédric RENAIS <cedric.renais@gmail.com>
2024-06-10 13:52:54 +0200

    V0.0 : Mise en place + Navbar & Header

---
+++ sources/js/index.js
@@ -0,0 +1,18 @@
+//*************//
+// MENU BURGER //
+//*************//
+// Selectionne les éléments de la barre de navigation
+const navbarIcon = document.querySelector('.navbar__icon');
+const navbarMenu = document.querySelector('.navbar__menu');
+// Ajoute un écouteur d'événements pour le clic sur l'icône de la barre de navigation pour activer le menu
+navbarIcon.addEventListener('click', () => {
+  navbarMenu.classList.toggle('active');
+});
+// Sélectionne les liens de la barre de navigation
+const navbarLinks = document.querySelectorAll('.navbar__link');
+// Ajoute un écouteur d'événements pour le clic sur les liens de la barre de navigation pour masquer le menu
+navbarLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    navbarMenu.classList.remove('active');
+  });
+});
